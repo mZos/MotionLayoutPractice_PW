0a9320cd634d5d80fb54b4e11c71b551d1734489
Mohammad Zakariya <mohammadzakariya@Mohammads-MacBook-Pro.local>
2022-05-02 14:52:52 +0530

    added dislike animation

--- app/src/main/java/com/zakariya/motionlayoutpractice/MainActivity.kt
+++ app/src/main/java/com/zakariya/motionlayoutpractice/MainActivity.kt
@@ -1,14 +1,26 @@
 package com.zakariya.motionlayoutpractice
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var button: Button
+    private lateinit var bottomSheet: MotionLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        button = findViewById(R.id.button)
+        bottomSheet = findViewById(R.id.constraintLayout2)
+
+        button.setOnClickListener {
+
+        }
     }
 
 }
