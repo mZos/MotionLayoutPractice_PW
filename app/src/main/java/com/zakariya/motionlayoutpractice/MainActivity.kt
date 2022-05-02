fc73a31c1a91aff410ff46428332a313b5c59022
Zakariya <mohdzakariya272@gmail.com>
2022-05-02 13:05:27 +0530

    motion layout complex animation

--- app/src/main/java/com/zakariya/motionlayoutpractice/MainActivity.kt
+++ app/src/main/java/com/zakariya/motionlayoutpractice/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.zakariya.motionlayoutpractice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
     }
+
 }
